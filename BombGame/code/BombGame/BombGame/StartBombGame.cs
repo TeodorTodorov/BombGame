946987433a168ee437febc4d9f17e247eb610e43
TeodorTodorov <teodorvulchev@gmail.com>
2014-07-25 10:11:33 +0300

    Revert "Revert "up""
    This reverts commit 0ba9fd9660e84a805f4131a3987c77ca36aeb3bb.

---
+++ BombGame/code/BombGame/BombGame/StartBombGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombGame
+{
+    class StartBombGame
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Handle exceptions,  add user input check
+            Console.WindowHeight = 14;
+            Console.SetBufferSize(80,14);
+
+        //
+            bool play = true;
+            GameCore game = new GameCore();
+            while (play = true)
+            {            
+              game.Draw();
+              Console.WriteLine();
+              Console.WriteLine("to restart the game type \"restart\"");
+              Console.WriteLine("give me something like /A1/: ");
+             
+              string reader = Console.ReadLine();
+              reader = reader.Trim();
+              if (reader == "restart")
+              {
+                  game.LoadGrounds();
+
+              }
+              else
+              {
+                  game.Checker(reader);
+              }
+            }
+
+           
+        }
+    }
+}
